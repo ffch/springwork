1a3519622fae28380ed22e2d023b0e2b8653abfc
cff <916881512@qq.com>
2017-12-26 13:37:25 +0800

    springwork-common公共项目

---
+++ springwork-common/src/main/java/com/cff/springwork/common/constant/Constant.java
@@ -0,0 +1,8 @@
+package com.cff.springwork.common.constant;
+
+public class Constant {
+	public final static String RESPONSE_OK = "0000"; 
+	public final static String USERTYPE = "1001"; 
+	public final static String COMMONAEAR = "0000"; 
+	public final static String USERSEQ = "userseq"; 
+}
