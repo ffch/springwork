ab1cd01f0e3ea6d9ba4e445602ed7e08faa89ff3
cff <916881512@qq.com>
2018-02-09 16:27:25 +0800

    增加序列号获取

--- springwork-mybatis/src/main/java/com/cff/springwork/mybatis/mapper/AppSeqMapper.java
+++ springwork-mybatis/src/main/java/com/cff/springwork/mybatis/mapper/AppSeqMapper.java
@@ -3,5 +3,5 @@ package com.cff.springwork.mybatis.mapper;
 
 public interface AppSeqMapper {
 	public int getNextSeq(String seqName);
-		
+	public int getNextSeqWithSize(String seqName,int size);
 }
